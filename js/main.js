d99b8f593b0e7a658f6ff44c734dfd4a69e6a0cc
Jacob Chademwiri <jchademwiri@gmail.com>
2019-10-06 20:18:14 +0200

    Made the website responsive to mobile devices and wide screens.

--- js/main.js
+++ js/main.js
@@ -12,3 +12,28 @@ function initMap() {
   //The maker, position at location
   const maker = new google.maps.Marker({ positin: loc, map: map });
 }
+
+// Sticky menu background
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
