91d26d46cec6ae76c09f1ee5793caac909a5c09f
Jacob Chademwiri <jchademwiri@gmail.com>
2019-10-07 05:54:06 +0200

    Added php file to subumit the contact form

---
+++ process.php
@@ -0,0 +1,14 @@
+<!-- <?php
+if (isset($_POST['email'])) {
+    //Email information
+    $admin_email='hello@playhousegraphix.co.za';
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $messege = $_POST['messege'];
+}
+
+//Send email
+mail($admin_email, 'New Form Submission', $messege. '- ' . $phone, 'From: ', $email);
+
+header('location: http://playhousegraphix.co.za/microfinacier/success.html'); -->
